909f0096c42f48905e146e7f29180421aa221db5
poda2k <podayasser101@gmail.com>
2023-05-20 19:34:16 +0300

    modification in DB

--- app.js
+++ app.js
@@ -89,21 +89,21 @@ app.use( course );
 
 // dd
 // Admin Account DON`T DELETE THIS ROW //
-mainuserdata.user.create({
-    Name : 'A nice person' ,
-    Email_Login : 'Admin@admin.com',
-    Password : 'onlyAdmin123',
-    user_type: 'admin',
-    Gender: 'male',
-    About_Me : 'I am the Admin Of this Website',
+// mainuserdata.user.create({
+//     Name : 'A nice person' ,
+//     Email_Login : 'Admin@admin.com',
+//     Password : 'onlyAdmin123',
+//     user_type: 'admin',
+//     Gender: 'male',
+//     About_Me : 'I am the Admin Of this Website',
 
-})
-.then((result)=>{
-    console.log(result);
-})
-.catch((err)=>{
-    console.log(err);
-});
+// })
+// .then((result)=>{
+//     console.log(result);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
 
 // Admin Account //
 
